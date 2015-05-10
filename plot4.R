75d065bb327cc5e62eb9a8049df684eebe22dc6f
mitkosz <dgo4@cornell.edu>
2015-05-10 00:28:04 -0400

    first version

---
+++ plot4.R
@@ -0,0 +1,50 @@
+#plot4.R plots the assignment plot4 into a plot4.png file into the working directory
+#it is assumend that the data file is in the working directory
+
+#read the data
+#the estimate for the size of the data is ~ 150MB
+#this is large but the RAM today is > 1 GB
+#will be reading the whole data
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.string = "?")
+
+#find the rows you want to subset
+# days: 1-Feb-2007 and 2-Feb-2007
+#the format of date column is : day/month/year
+#"1/2/2007" selects also  "11/2/2007"...need to specify the begging of the line with ^
+index <- grep("^1/2/2007|^2/2/2007", data$Date)
+length(index) #there are 2880 rows that meet the criteria
+
+#subset the origina data frame
+feb1 <- data[index,]
+
+#chek for NA
+l <- is.na(feb) # and then table(l) -> gives 25920 FALSE = (2880*9), so no NA entries
+#do complete cases just in case that the data changes
+feb <- feb1[complete.cases(feb1),] 
+
+#convert in Date and Time into date format
+DateTime <- paste(feb$Date, feb$Time, sep = " ")
+DateTime <- strptime(DateTime, "%d/%m/%Y %H:%M:%S")
+
+#add a new column to the date frame with date formated time
+feb <- cbind(feb, DateTime)
+
+#make the histogram and print it in pnd file
+png("plot4.png")#png default height is 480 and default width is 480 pixels, no need to specify explicitly
+par(mfrow = c(2,2))
+with(feb,{
+  plot(DateTime, Global_active_power, type = "n", ylab = "Gloal Active Power", xlab = "")
+  lines(DateTime, Global_active_power)
+  plot(DateTime, Voltage, type = "n", ylab = "Voltage", xlab = "datetime")
+  lines(DateTime, Voltage)
+  plot(DateTime, Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = "")
+  lines(DateTime, Sub_metering_1, col = "black")
+  lines(DateTime, Sub_metering_2, col = "red")
+  lines(DateTime, Sub_metering_3, col = "blue")
+  legend("topright", legend =c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", lty = 1, col = c("black", "red", "blue"))
+  plot(DateTime, Global_reactive_power, type = "n", ylab = "Gloal_rective_power", xlab = "datetime")
+  lines(DateTime, Global_reactive_power)
+})
+dev.off()
+
+
